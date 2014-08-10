472e7ae6afcfba43839824d2980c889ab2048669
Connor Hindley <conn.hindley@gmail.com>
2014-08-10 14:15:09 -0400

    connect to MongoHQ

--- db.js
+++ db.js
@@ -4,9 +4,8 @@ var mongoose = require('mongoose');
 require('./models/Post');
 
 var mongoDbURL = 'mongodb://localhost/thedigest';
-if (process.env.VCAP_SERVICES) {
-    var env = JSON.parse(process.env.VCAP_SERVICES);
-    mongoDbURL = env.mongolab[0].credentials.uri;
+if (process.env.MONGOHQ_URL) {
+    mongoDbURL = process.env.MONGOHQ_URL;
 }
 
 mongoose.connect(mongoDbURL);
