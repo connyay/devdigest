75d69ad1ce9c676aa88bf4fd78d4a2b7e158541b
Connor Hindley <conn.hindley@gmail.com>
2014-08-10 12:42:33 -0400

    sort by _id

--- api/post.js
+++ api/post.js
@@ -3,6 +3,9 @@ var Post = require('mongoose').model('Post');
 
 exports.index = function(req, res, next) {
     Post.find()
+        .sort({
+            '_id': 1
+        })
         .exec(function(err, posts) {
             if (err) {
                 return next(err);
