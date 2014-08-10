f66e65b0deb93c7e6715f219b6c296cc7082eeb6
Connor Hindley <conn.hindley@gmail.com>
2014-08-10 14:06:53 -0400

    add loading directive

--- public/app.js
+++ public/app.js
@@ -3,9 +3,17 @@
     angular.module('thedigest', [])
         .controller('MainCtrl', ['$scope', '$http',
             function($scope, $http) {
+                $scope.loading = true;
                 $http.get('/posts').success(function(posts){
                     $scope.posts = posts;
+                    $scope.loading = false;
                 });
             }
-        ]);
+        ])
+        .directive('loading', function() {
+            return {
+                restrict: 'E',
+                template: '<div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>'
+            };
+        });
 })();
