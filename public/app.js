7b89696c9d81a929aa702d094fbf629d28688433
Connor Hindley <conn.hindley@gmail.com>
2014-08-10 12:16:12 -0400

    load posts

--- public/app.js
+++ public/app.js
@@ -2,6 +2,10 @@
     'use strict';
     angular.module('thedigest', [])
         .controller('MainCtrl', ['$scope', '$http',
-            function($scope, $http) {}
+            function($scope, $http) {
+                $http.get('/posts').success(function(posts){
+                    $scope.posts = posts;
+                });
+            }
         ]);
 })();
