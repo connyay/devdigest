46e5fa5fc59c12f1b3f411a63fe2280d13a90cf9
Connor Hindley <conn.hindley@gmail.com>
2014-08-31 11:41:14 -0400

    I need to decide on a name

--- cache.js
+++ cache.js
@@ -2,10 +2,10 @@
 
 var memjs = require('memjs');
 
-if(process.env.MEMCACHEDCLOUD_SERVERS) {
+if (process.env.MEMCACHEDCLOUD_SERVERS) {
     exports.client = memjs.Client.create(process.env.MEMCACHEDCLOUD_SERVERS, {
-      username: process.env.MEMCACHEDCLOUD_USERNAME,
-      password: process.env.MEMCACHEDCLOUD_PASSWORD
+        username: process.env.MEMCACHEDCLOUD_USERNAME,
+        password: process.env.MEMCACHEDCLOUD_PASSWORD
     });
 } else {
     exports.client = memjs.Client.create('127.0.0.1:11211');
--- db.js
+++ db.js
@@ -3,7 +3,7 @@
 var mongoose = require('mongoose');
 require('./models/Post');
 
-var mongoDbURL = 'mongodb://localhost/thedigest';
+var mongoDbURL = 'mongodb://localhost/dgst-it';
 if (process.env.MONGOHQ_URL) {
     mongoDbURL = process.env.MONGOHQ_URL;
 }
