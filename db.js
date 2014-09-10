ed6dd3b5f11878c1b9b13803ca580e0d23471711
Connor Hindley <conn.hindley@gmail.com>
2014-09-09 21:59:03 -0400

    add openshift db

--- db.js
+++ db.js
@@ -7,5 +7,8 @@ var mongoDbURL = 'mongodb://localhost/dgst-it';
 if (process.env.MONGOHQ_URL) {
     mongoDbURL = process.env.MONGOHQ_URL;
 }
+if (process.env.OPENSHIFT_MONGODB_DB_URL) {
+    mongoDbURL = process.env.OPENSHIFT_MONGODB_DB_URL;
+}
 
 mongoose.connect(mongoDbURL);
